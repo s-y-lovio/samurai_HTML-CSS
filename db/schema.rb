5d2a997b63e3772e1a87c688b90563e5d0900459
s-y-lovio <s.y.lovio@gmail.com>
2021-09-24 12:48:56 +0900

    04 create products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_095038) do
+ActiveRecord::Schema.define(version: 2021_09_24_005919) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 2021_09_22_095038) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
