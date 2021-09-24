c3d6a2b35f33f413edbe60fc19cf97e80d178300
s-y-lovio <s.y.lovio@gmail.com>
2021-09-24 13:58:31 +0900

    05 create login system

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Samazon
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +20,7 @@ module Samazon
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # バリデーションエラーが発生したときに'field_with_errors'というclassを付加しない
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
