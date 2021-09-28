6d07ffefa72365ada7637c9d6762a40c2755d046
s-y-lovio <s.y.lovio@gmail.com>
2021-09-28 15:59:39 +0900

    deproy 9/28 3

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
 
   namespace :dashboard do
+    resources :users, only: [:index, :destroy]
     resources :major_categories, except: [:new]
     resources :categories, except: [:new]
     resources :products, except: [:show]
@@ -44,6 +45,7 @@ Rails.application.routes.draw do
       get "mypage/edit_password", :to =>"users#edit_password"
       put "mypage/password", :to => "users#update_password"
       get "mypage/favorite", :to => "users#favorite"
+      delete "mypage/delete", :to => "users#destroy"
     end
   end
 
