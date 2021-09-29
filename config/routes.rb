3cac8d70e07400a7c38489b71b4204bbdabc4e3f
s-y-lovio <s.y.lovio@gmail.com>
2021-09-29 17:19:33 +0900

    deproy 9/29 1

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,10 @@ Rails.application.routes.draw do
       put "mypage/password", :to => "users#update_password"
       get "mypage/favorite", :to => "users#favorite"
       delete "mypage/delete", :to => "users#destroy"
+      get "mypage/cart_history", :to => "users#cart_history_index", :as => "mypage_cart_histories"
+      get    "mypage/cart_history/:num", :to => "users#cart_history_show", :as => "mypage_cart_history"
+      get    "mypage/register_card",     :to => "users#register_card"
+      post   "mypage/token",             :to => "users#token"
     end
   end
 
